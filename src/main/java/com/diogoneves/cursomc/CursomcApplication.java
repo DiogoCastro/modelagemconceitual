845e999486149c0a2c60bc50902aba9aeeed3ba6
Diogo Neves <diogooncastro1@gmail.com>
2017-10-11 13:50:40 -0300

    Criando operação de instanciação.

--- src/main/java/com/diogoneves/cursomc/CursomcApplication.java
+++ src/main/java/com/diogoneves/cursomc/CursomcApplication.java
@@ -1,12 +1,32 @@
 package com.diogoneves.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.diogoneves.cursomc.domain.Categoria;
+import com.diogoneves.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");  // Está nulo pois o id é implementado automaticamente
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.save(Arrays.asList(cat1, cat2));  // Salva as categorias do banco
+		
 	}
 }
