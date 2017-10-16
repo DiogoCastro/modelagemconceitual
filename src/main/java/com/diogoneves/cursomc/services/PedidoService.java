02b4a3497dc06d2551d5ec92462944458431849c
Diogo Neves <diogooncastro1@gmail.com>
2017-10-16 17:32:49 -0200

    Endpoin/pedidos/{id} disponibilizado

---
+++ src/main/java/com/diogoneves/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.diogoneves.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diogoneves.cursomc.domain.Pedido;
+import com.diogoneves.cursomc.repositories.PedidoRepository;
+import com.diogoneves.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired  // Permite a dependência a ser automaticamente instanciada pelo Spring
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		 Pedido obj = repo.findOne(id);
+		 if (obj == null) {
+			 throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + " Tipo: " + Pedido.class.getName());
+		 }
+		 return obj;
+	}	
+	
+}
