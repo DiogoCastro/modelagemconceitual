60b4a173e4c95c32cedd22bcf212ce7ea96cf76a
Diogo Neves <diogooncastro1@gmail.com>
2017-10-19 13:19:45 -0200

    Validação sintática com Bean Validation

--- src/main/java/com/diogoneves/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/diogoneves/cursomc/dto/CategoriaDTO.java
@@ -2,12 +2,18 @@ package com.diogoneves.cursomc.dto;
 
 import java.io.Serializable;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
 import com.diogoneves.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {  // Classe para garantir que apenas as categorias apareçam, impedindo que apareçam outras listas relacionadas a elas.
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {}
--- src/main/java/com/diogoneves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/diogoneves/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -35,14 +37,16 @@ public class CategoriaResource {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDto) {
+		Categoria obj = service.FromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {  // Combinação dos dois últimos, com @RequestBody e @PathVariable
+	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDto, @PathVariable Integer id) {  // Combinação dos dois últimos, com @RequestBody e @PathVariable
+		Categoria obj = service.FromDTO(objDto);
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
