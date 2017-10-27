5a068b146dd587a07e8f765976ea3d507d561f3d
Diogo Neves <diogooncastro1@gmail.com>
2017-10-27 12:35:18 -0200

    Criando profile de desenvolvimento

--- src/main/java/com/diogoneves/cursomc/CursomcApplication.java
+++ src/main/java/com/diogoneves/cursomc/CursomcApplication.java
@@ -1,26 +1,19 @@
 package com.diogoneves.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.diogoneves.cursomc.services.DBService;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	public static void main(String[] args) {
-		
-		
-		
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 	
-	@Autowired
-	DBService service;
+	
 	
 	@Override
 	public void run(String... args) throws Exception {
-		service.instantiateTestDatabase();
+		
 	}
 }
